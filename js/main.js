d98b3d29cdb7ac6efd2994d8249a866e14a6c318
GioGio4 <g.ceccobelli@outlook.it>
2023-01-24 18:00:39 +0100

    Complete function

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
 const price_km = 0.21;
 
 const user_age = (document.getElementById("user-age"));
-console.log(typeof user_age);
+// console.log(typeof user_age);
 
 const user_km = (document.getElementById("user-km"));
-console.log(typeof user_km);
+// console.log(typeof user_km);
 
 const submit_button = document.querySelector("#my-button")
 
@@ -18,8 +18,34 @@ submit_button.addEventListener(
     "click",
     function () {
 
-        let outpout_msg = (user_km.value * price_km);
-        document.getElementById("price-bill").innerHTML = (outpout_msg);
+        // Sconto Young
+        if (user_age.value < 18) {
+
+            let discount_young = ((price_km * user_km.value) * 20 / 100);
+            console.log("sconto young " + discount_young);
+
+            let outpout_msg = (user_km.value * price_km) - discount_young;
+            document.getElementById("price-bill").innerHTML = ("€ " + outpout_msg.toFixed(2));
+        }
+
+        // Sconto Over
+        else if (user_age.value > 65) {
+
+            let discount_over = ((price_km * user_km.value) * 40 / 100);
+            console.log("sconto over " + discount_over);
+
+            let outpout_msg = (user_km.value * price_km) - discount_over;
+            document.getElementById("price-bill").innerHTML = ("€ " + outpout_msg.toFixed(2));
+
+        }
+
+        // Nessuno sconto
+        else {
+            let outpout_msg = (user_km.value * price_km);
+            document.getElementById("price-bill").innerHTML = ("€ " + outpout_msg.toFixed(2));
+        }
+
+
     }
 
 
